0be58cfb4852be1bee2122de067ff2ab56dba429
Jonel Briones <jonel.c.briones@gmail.com>
2022-05-20 05:40:19 -0700

    added client side log & reg form

---
+++ client/src/components/Registration.js
@@ -0,0 +1,71 @@
+import React from 'react';
+
+const Registration = (props) => {
+    const {onSubmitHandler,employee,errors,onChangeHandler,buttonText,confirmReg} = props;
+    return (
+        <>
+        <form onSubmit={onSubmitHandler}>
+            <div>
+                {
+                    confirmReg?
+                <h1>{confirmReg}</h1>:null
+                }
+            </div>
+            <div>
+                <label>
+                    {
+                    errors.firstName?
+                    <span>{errors.firstName.message}</span>:
+                    <span>First Name</span>
+                    }
+                </label>
+                <input type="text" name="firstName" value={employee.firstName} onChange={(e)=>onChangeHandler(e)}></input>
+            </div>
+            <div>
+            <label>
+                    {
+                    errors.lastName?
+                    <span>{errors.lastName.message}</span>:
+                    <span>Last Name</span>
+                    }
+                </label>
+                <input type="text" name="lastName" value={employee.lastName} onChange={(e)=>onChangeHandler(e)}></input>
+            </div>
+            <div>
+            <label>
+                    {
+                    errors.username?
+                    <span>{errors.username.message}</span>:
+                    <span>Username</span>
+                    }
+                </label>
+                <input type="text" name="username" value={employee.username} onChange={(e)=>onChangeHandler(e)}></input>
+            </div>
+            <div>
+            <label>
+                    {
+                    errors.password?
+                    <span>{errors.password.message}</span>:
+                    <span>Password</span>
+                    }
+                </label>
+                <input type="text" name="password" value={employee.password} onChange={(e)=>onChangeHandler(e)}></input>
+            </div>
+            <div>
+            <label>
+                    {
+                    errors.confirmPassword?
+                    <span>{errors.confirmPassword.message}</span>:
+                    <span>Confirm Password</span>
+                    }
+                </label>
+                <input type="text" name="confirmPassword" value={employee.confirmPassword} onChange={(e)=>onChangeHandler(e)}></input>
+            </div>
+            <div>
+                <button type="submit">{buttonText}</button>
+            </div>
+        </form>
+        </>
+    )
+}
+export default Registration;
---
+++ client/src/view/LogReg.js
@@ -0,0 +1,96 @@
+import React, {useState,useEffect} from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import Registration from '../components/Registration';
+import Login from '../components/Login';
+const LogReg = () => {
+    const navigate = useNavigate();
+    const [confirmReg,setConfirmReg] = useState('')
+    const [errors,setError] = useState({})
+    const [employee,setEmployee] = useState({
+        firstName:"",
+        lastName:"",
+        username:"",
+        password:"",
+        confirmPassword:""
+    })
+
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+        axios.post('http://localhost:8000/api/employee/register',employee)
+            .then((res)=>{
+                console.log(res.data)
+                setEmployee({
+                    firstName:"",
+                    lastName:"",
+                    username:"",
+                    password:"",
+                    confirmPassword:""
+                })
+                setConfirmReg("Thank you for Registering, you can now log in!")
+                setError({})
+            })
+            .catch((err)=>{
+                console.log(err.response.data.errors)
+                setError(err.response.data.errors)
+            })
+    }
+    
+    const onChangeHandler = (e) => {
+        const newEmployeeObject = {...employee};
+        newEmployeeObject[e.target.name] = e.target.value;
+        console.log(newEmployeeObject)
+        setEmployee(newEmployeeObject)
+    }
+
+    // Login
+    const [loginErrors,setLoginError] = useState("")
+    const [employeeLogin,setEmployeeLogin] = useState({
+        username: "",
+        password: ""
+    })
+
+    const onSubmitHandlerLogin = (e) => {
+        e.preventDefault()
+        axios.post('http://localhost:8000/api/employee/login',employeeLogin)
+            .then((res)=>{
+                console.log(res.data)
+                navigate('/home');
+            })
+            .catch((err)=>{
+                console.log(err.response.data.message)
+                setLoginError(err.response.data.message)
+                setEmployeeLogin({
+                    username:"",
+                    password:""
+                })
+            })
+    }
+
+    const onChangeHandlerLogin = (e) => {
+        const employeeObject = {...employeeLogin};
+        employeeObject[e.target.name] = e.target.value;
+        console.log(employeeObject)
+        setEmployeeLogin(employeeObject)
+    }
+    return (
+        <div>
+            <Registration
+            onSubmitHandler={onSubmitHandler}
+            onChangeHandler={onChangeHandler}
+            employee={employee}
+            errors={errors}
+            confirmReg={confirmReg}
+            buttonText={'Create Account'}
+            />
+            <Login
+                onSubmitHandler={onSubmitHandlerLogin}
+                onChangeHandler={onChangeHandlerLogin}
+                employee={employeeLogin}
+                errors={loginErrors}
+                buttonText={'Login'}
+            />
+        </div>
+    )
+}
+export default LogReg;
