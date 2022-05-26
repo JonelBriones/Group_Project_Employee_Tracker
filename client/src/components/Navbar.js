3f5689a7b2bbc927f00e978309d3b34dacc7a734
Jonel Briones <jonel.c.briones@gmail.com>
2022-05-26 03:29:39 -0700

    added link to view logged in account

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -31,25 +31,10 @@ const Navbar = () => {
         <li className="nav-item">
           <Link className="nav-link active" aria-current="page" to="/completed_task">Completed Task</Link>
         </li>
-        {/* <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a className="dropdown-item" href="#">Action</a></li>
-            <li><a className="dropdown-item" href="#">Another action</a></li>
-            <li><hr className="dropdown-divider"/></li>
-            <li><a className="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li> */}
         <li className="nav-item">
-          <a className="nav-link disabled">Logged in as {employee.firstName + " " + employee.lastName}</a>
+          <Link to={`/view/employee/${employee._id}`}className="nav-link">Logged in as {employee.firstName + " " + employee.lastName}</Link>
         </li>
       </ul>
-      {/* <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form> */}
     </div>
   </div>
 </nav>
